5a3a41a6de5e30231624d077c92e355a6332dd0c
WilliamTangq <tanstrifelife@gmail.com>
2026-06-20 16:06:10 +1000

    add web analyst

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 import { AuthProvider } from "./components/auth-provider";
 import { LanguageProvider } from "./components/language-provider";
 import "./globals.css";
